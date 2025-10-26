3c959f2cf3c031048de79459edf9f04077c79653
Viktoriia <viktoriiabieliaieva91@gmail.com>
2025-10-26 21:59:21 +0100

    add tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const formElem = document.querySelector(".login-form");
+formElem.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const emailValue = form.elements.email.value.trim();
+    const passwordValue = form.elements.password.value.trim();
+    const emailName = form.elements.email.name;
+    const passwordName = form.elements.password.name;
+    
+    
+    if (emailValue === '' || passwordValue === '') {
+        return alert('All form fields must be filled in')
+    };
+
+    const user = {
+        [emailName]: emailValue,
+        [passwordName]: passwordValue,
+    }
+    console.log(user);
+    form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const spanEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", (event) => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;
+})
